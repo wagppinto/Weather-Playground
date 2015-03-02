30229cedae2622258bd10603619d1e8a3852903a
wagppinto <wagpinto@me.com>
2015-03-02 14:27:47 -0700

    Add Model Class Weather

---
+++ Weather Playground/WPWeather.h
@@ -0,0 +1,13 @@
+//
+//  WPWeather.h
+//  Weather Playground
+//
+//  Created by Wagner Pinto on 3/2/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WPWeather : NSObject
+
+@end
---
+++ Weather Playground/WPWeather.m
@@ -0,0 +1,13 @@
+//
+//  WPWeather.m
+//  Weather Playground
+//
+//  Created by Wagner Pinto on 3/2/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "WPWeather.h"
+
+@implementation WPWeather
+
+@end
